a9e58abbaac5b1be78a91cbaab20a4e31a3a853a
Christopher N. Hesse <raymanfx@gmail.com>
2019-08-23 15:21:54 +0200

    lib: face: Improve HoG dlib face detector
    Do not perform any pyramid level scaling here, this should
    be done by callers before passing the image to the actual
    detector if needed.
    
    Only ever process grayscale images. Convert to grayscale
    straight away in the case of BGR inputs.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- src/lib/face/src/hog_face_detector.cpp
+++ src/lib/face/src/hog_face_detector.cpp
@@ -27,7 +27,6 @@ HOGFaceDetector::~HOGFaceDetector() {
 
 bool HOGFaceDetector::detect_faces(const Image &img, std::vector<Polygon<>> &faces) {
     // http://dlib.net/face_detection_ex.cpp.html
-    dlib::array2d<dlib::bgr_pixel> dlib_bgr_image;
     dlib::array2d<unsigned char> dlib_gray_image;
     std::vector<dlib::rectangle> dets;
     cv::Mat mat;
@@ -43,27 +42,7 @@ bool HOGFaceDetector::detect_faces(const Image &img, std::vector<Polygon<>> &fac
         dlib::assign_image(dlib_gray_image, dlib::cv_image<unsigned char>(mat));
         break;
     case 3:
-        dlib::assign_image(dlib_bgr_image, dlib::cv_image<dlib::bgr_pixel>(mat));
-        break;
-    default:
-        return false;
-    }
-
-    // Make the image bigger by a factor of two.  This is useful since
-    // the face detector looks for faces that are about 80 by 80 pixels
-    // or larger.  Therefore, if you want to find faces that are smaller
-    // than that then you need to upsample the image as we do here by
-    // calling pyramid_up().  So this will allow it to detect faces that
-    // are at least 40 by 40 pixels in size.  We could call pyramid_up()
-    // again to find even smaller faces, but note that every time we
-    // upsample the image we make the detector run slower since it must
-    // process a larger image.
-    switch (mat.channels()) {
-    case 1:
-        dlib::pyramid_up(dlib_gray_image);
-        break;
-    case 3:
-        dlib::pyramid_up(dlib_bgr_image);
+        dlib::assign_image(dlib_gray_image, dlib::cv_image<dlib::bgr_pixel>(mat));
         break;
     default:
         return false;
@@ -71,16 +50,7 @@ bool HOGFaceDetector::detect_faces(const Image &img, std::vector<Polygon<>> &fac
 
     // Now tell the face detector to give us a list of bounding boxes
     // around all the faces it can find in the image.
-    switch (mat.channels()) {
-    case 1:
-        dets = m_detector(dlib_gray_image);
-        break;
-    case 3:
-        dets = m_detector(dlib_bgr_image);
-        break;
-    default:
-        return false;
-    }
+    dets = m_detector(dlib_gray_image);
 
     for (auto const &box : dets) {
         faces.emplace_back(Polygon<>(
