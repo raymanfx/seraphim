de5ad79091f032ff92688160bbf3674f36014405
Christopher N. Hesse <raymanfx@gmail.com>
2019-10-19 12:59:56 +0200

    lib: core: Implement threading helpers
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

---
+++ src/lib/core/include/seraphim/threading.h
@@ -0,0 +1,91 @@
+/*
+ * (C) Copyright 2019
+ * The Seraphim Project Developers.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef SPH_CORE_THREADING_H
+#define SPH_CORE_THREADING_H
+
+#include <mutex>
+
+namespace sph {
+
+/**
+ * @brief Thin wrapper to synchronize an entire object.
+ *
+ * Note that this does not synchronize all access to the wrapped instance, but only guards the
+ * access of separate threads to the same instance of this class or access from multiple instances
+ * of this class to the wrapped instance, regardless of threading.
+ */
+template <class T> class Synchronized final {
+public:
+    /**
+     * @brief Wrapper instance providing synchronized access.
+     *
+     * To be able to use this thin wrapper, a class has to implement the @ref Synchronizeable
+     * marker interface.
+     *
+     * @param parent The "real" instance to be synchronized.
+     */
+    explicit Synchronized(T &parent) : m_ref(parent), m_lock(parent.m_mutex) {}
+
+    /**
+     * @brief Default move constructor.
+     */
+    Synchronized(Synchronized &&) = default;
+
+    /**
+     * @brief Pass through class member access to the "real" instance.
+     * @return Pointer to the locked class instance.
+     */
+    T *operator->() noexcept { return &m_ref; }
+
+private:
+    Synchronized(const Synchronized &) = delete;
+    Synchronized &operator=(const Synchronized &) = delete;
+    Synchronized &operator=(Synchronized &&) = delete;
+
+    /// Reference to the parent class to wire up class member access.
+    T &m_ref;
+
+    /// Locks the parent classes mutex.
+    std::unique_lock<std::mutex> m_lock;
+};
+
+/**
+ * @brief Marker interface for thread safe instance access.
+ *
+ * Classes implementing this interface can be accessed in an atomic fashion.
+ * This facilitates easy thread safe classes.
+ */
+template <class T> class Synchronizeable {
+public:
+    virtual ~Synchronizeable() = default;
+
+    /**
+     * @brief Thread safe access to the instance.
+     *
+     * All actions on the class which go through this thin wrapper are atomic, regardless of which
+     * thread they originate from.
+     *
+     * @return The synchronized instance which is locked internally.
+     */
+    Synchronized<T> synchronized() { return Synchronized<T>(*(static_cast<T *>(this))); }
+
+protected:
+    /// Since this is a marker interface, disallow arbitrary instantiation.
+    Synchronizeable() = default;
+
+private:
+    /// the @ref Synchronized wrapper must have access to the mutex
+    friend class Synchronized<T>;
+
+    /// mutex used by the @ref Synchronized wrapper
+    std::mutex m_mutex;
+};
+
+} // namespace sph
+
+#endif // SPH_CORE_THREADING_H
---
+++ tests/core/threading.cpp
@@ -0,0 +1,82 @@
+#include <catch2/catch.hpp>
+#include <thread>
+
+#include <seraphim/threading.h>
+
+using namespace sph;
+
+class Counter : public Synchronizeable<Counter> {
+public:
+    Counter() {}
+
+    inline int val() const { return m_val; }
+
+    void inc() {
+        m_val++;
+
+        if (!m_last_op_inc) {
+            m_consecutive_incs = 0;
+        }
+        m_last_op_inc = true;
+        m_consecutive_incs++;
+    }
+
+    void dec() {
+        m_val--;
+
+        if (m_last_op_inc) {
+            m_consecutive_decs = 0;
+        }
+        m_last_op_inc = false;
+        m_consecutive_decs++;
+    }
+
+    inline int consecutive_incs() const { return m_consecutive_incs; }
+    inline int consecutive_decs() const { return m_consecutive_decs; }
+
+private:
+    int m_val = 0;
+
+    bool m_last_op_inc = false;
+    int m_consecutive_incs = 0;
+    int m_consecutive_decs = 0;
+};
+
+TEST_CASE( "Synchronizeable runtime behavior", "[Synchronizeable]" ) {
+    SECTION( "all members of the implementing class can be accessed" ) {
+        Counter cnt;
+
+        // this is a compile time test
+        cnt.synchronized()->val();
+        cnt.synchronized()->inc();
+        cnt.synchronized()->dec();
+        cnt.synchronized()->consecutive_incs();
+        cnt.synchronized()->consecutive_decs();
+    }
+}
+
+TEST_CASE( "Synchronized runtime behavior", "[Synchronized]" ) {
+    SECTION( "all access to the wrapper instance is thread safe" ) {
+        Counter cnt;
+
+        std::thread inc_thread([&]() {
+            Synchronized<Counter> scnt(cnt);
+            for (auto i = 0; i < 1000; i++) {
+                scnt->inc();
+            }
+        });
+
+        std::thread dec_thread([&]() {
+            Synchronized<Counter> scnt(cnt);
+            scnt->dec();
+            scnt->dec();
+        });
+
+        inc_thread.join();
+        dec_thread.join();
+
+        // verify that all ops occured in order
+        REQUIRE( cnt.consecutive_incs() == 1000 );
+        REQUIRE( cnt.consecutive_decs() == 2 );
+    }
+}
