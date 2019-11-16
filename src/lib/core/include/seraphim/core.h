c8686d40595dbbb14468937d38355de6df50d4ce
Christopher N. Hesse <raymanfx@gmail.com>
2019-11-16 18:13:38 +0100

    lib: core: Provide a unified header
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

---
+++ src/lib/core/include/seraphim/core.h
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2019
+ * The Seraphim Project Developers.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef SPH_CORE_H
+#define SPH_CORE_H
+
+#include "computable.h"
+#include "except.h"
+#include "image.h"
+#include "image_converter.h"
+#include "matrix.h"
+#include "memory.h"
+#include "module.h"
+#include "pixelformat.h"
+#include "point.h"
+#include "polygon.h"
+#include "size.h"
+#include "threading.h"
+
+#endif // SPH_CORE_H
