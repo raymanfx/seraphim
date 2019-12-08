f3a6c08e745304cd82bf48e41caaf8ecb7a230f8
Christopher N. Hesse <raymanfx@gmail.com>
2019-12-08 17:06:38 +0100

    lib: face: Dynamically add opencv/dlib components
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- src/lib/face/include/seraphim/face.h
+++ src/lib/face/include/seraphim/face.h
@@ -11,11 +11,17 @@
 #include <seraphim/face/face_detector.h>
 #include <seraphim/face/face_recognizer.h>
 #include <seraphim/face/facemark_detector.h>
-#include <seraphim/face/hog_face_detector.h>
-#include <seraphim/face/kazemi_facemark_detector.h>
+
+#ifdef WITH_OPENCV
 #include <seraphim/face/lbf_facemark_detector.h>
 #include <seraphim/face/lbp_face_detector.h>
 #include <seraphim/face/lbp_face_recognizer.h>
 #include <seraphim/face/utils.h>
+#endif
+
+#ifdef WITH_DLIB
+#include <seraphim/face/hog_face_detector.h>
+#include <seraphim/face/kazemi_facemark_detector.h>
+#endif
 
 #endif // SPH_FACE_H
